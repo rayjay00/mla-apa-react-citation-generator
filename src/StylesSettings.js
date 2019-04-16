809099deab90e4570acd9562be9d6c333e10e859
Ryan <hello@ryanjordal.me>
2019-04-15 21:44:00 -0400

    style settings

---
+++ src/StylesSettings.js
@@ -0,0 +1,15 @@
+export const stylesettings = {
+    mla: {
+        primary: "#55acee",
+        secondary: "#2ecc71",
+        secondaryhover: "#0AC25A",
+        secondaryboxshadow: "#15B358"
+    },
+    apa: {
+        primary: "#9b59b6",
+        secondary: "#BD85D3",
+        secondaryhover: "#813A9D",
+        secondaryboxshadow: "#6F1F8E"
+    }
+}
+
