9790d3af973e507180001bdf8aec537fb12cc827
Ryan <hello@ryanjordal.me>
2019-06-07 08:46:03 -0400

    accessibility and modal button

--- src/Modal.js
+++ src/Modal.js
@@ -1,41 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
-import { stylesettings } from './StylesSettings';
 import Citation from './Citation';
+import { StyledSubmitButton, stylesettings } from './StylesSettings';
 
 const StyledModal = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: ${ stylesettings.extra.modalbg };
-    padding: 1rem 0.5rem;
-    border-radius: 10px;
-`
-
-export const StyledListHeading = styled.h2`
-    color: white;
-    display: inline;
-    padding-left: 1rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: ${stylesettings.extra.modalbg};
+  padding: 1rem 0.5rem;
+  border-radius: 10px;
 `;
 
-
-const StyledClose = styled.span`
-    font-size: 1.5rem;
-    color: white;
-    font-weight: bold;
-`
-//I've decided to destructure the "style" prop although it is only used for the styled component. 
+export const StyledListHeading = styled.h2`
+  color: white;
+  display: inline;
+  padding-left: 1rem;
+`;
+//I've decided to destructure the "style" prop although it is only used for the styled component.
 //Want to learn more about what to do there in the future
 const Modal = ({ modalClosed, citation, style }) => {
-
-    return (
-        <StyledModal>
-            <StyledClose onClick={ modalClosed }>x</StyledClose>
-            <StyledListHeading>Here's your citation!</StyledListHeading>
-            <Citation citationText={ citation } style={ style } />
-        </StyledModal>
-    )
-}
+  return (
+    <StyledModal
+      role="dialog"
+      aria-labelledby="dialog1Title"
+      aria-describedby="dialog1Desc"
+    >
+      <StyledListHeading id="dialogTitle">
+        Here's your citation!
+      </StyledListHeading>
+      <Citation id="dialog1Desc" citationText={citation} style={style} />
+      <StyledSubmitButton onClick={modalClosed}>x</StyledSubmitButton>
+    </StyledModal>
+  );
+};
 
 export default Modal;
