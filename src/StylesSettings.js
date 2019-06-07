a35c92aeeb6a6f7d71859623bd3a52068d39ccb6
Ryan <hello@ryanjordal.me>
2019-06-07 08:46:13 -0400

    moved button and changed colors

--- src/StylesSettings.js
+++ src/StylesSettings.js
@@ -1,18 +1,36 @@
+import styled from 'styled-components';
 export const stylesettings = {
-    mla: {
-        primary: "#55acee",
-        secondary: "#2ecc71",
-        secondaryhover: "#0AC25A",
-        secondaryboxshadow: "#15B358"
-    },
-    apa: {
-        primary: "#9b59b6",
-        secondary: "#BD85D3",
-        secondaryhover: "#813A9D",
-        secondaryboxshadow: "#6F1F8E"
-    },
-    extra: {
-        modalbg: "#d28484"
-    }
-}
+  mla: {
+    primary: '#555aee',
+    secondary: '#ee5555',
+    secondaryhover: '#0AC25A',
+    secondaryboxshadow: '#15B358'
+  },
+  apa: {
+    primary: '#9b59b6',
+    secondary: 'white',
+    secondaryhover: '#813A9D',
+    secondaryboxshadow: '#6F1F8E'
+  },
+  extra: {
+    modalbg: '#6c6abb'
+  }
+};
 
+export const StyledSubmitButton = styled.button`
+  border-radius: 5px;
+  padding: 15px 25px;
+  font-size: 22px;
+  text-decoration: none;
+  margin: 20px auto;
+  color: ${props => (props.onClick ? `black` : `white`)};
+  position: relative;
+  border: 0;
+  display: flex;
+  background-color: ${props =>
+    props.onClick ? `white` : stylesettings.mla.primary};
+  &:active {
+    transform: translate(0px, 5px);
+    box-shadow: 0px 1px 0px 0px;
+  }
+`;
