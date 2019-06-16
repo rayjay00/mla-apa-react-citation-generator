117d90c7a43e7e3e9af21b20790d2122cbdb7862
Ryan <hello@ryanjordal.me>
2019-06-16 11:05:17 -0400

    added proptypes

---
+++ src/Choose.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Radio from './Radio';
+
+const StyledRadioWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  font-weight: bold;
+`;
+
+const StyledHeading = styled.h1`
+  color: white;
+`;
+
+const Choose = ({ chooseStyle, style }) => {
+  return (
+    <StyledRadioWrapper>
+      <StyledHeading>{style} Book Citation</StyledHeading>
+      <Radio chooseStyle={chooseStyle} style={style} />
+    </StyledRadioWrapper>
+  );
+};
+
+Choose.propTypes = {
+  chooseStyle: PropTypes.string.isRequired,
+  style: PropTypes.string.isRequired
+};
+
+export default Choose;
