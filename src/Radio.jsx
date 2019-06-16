78706dafa0f3add50f0764400faa863683a3d71a
Ryan <hello@ryanjordal.me>
2019-06-16 11:08:09 -0400

    proptypes added

---
+++ src/Radio.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const StyledRadioInput = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Radio = ({ chooseStyle, style }) => (
+  <StyledRadioInput>
+    <label htmlFor='MLA'>
+      MLA
+      <input
+        type='radio'
+        name='MLA'
+        id='MLA'
+        value='MLA'
+        checked={style === 'MLA'}
+        onChange={chooseStyle}
+      />
+    </label>
+    <label htmlFor='APA'>
+      APA
+      <input
+        type='radio'
+        name='APA'
+        id='Chigago'
+        value='APA'
+        checked={style === 'APA'}
+        onChange={chooseStyle}
+      />
+    </label>
+  </StyledRadioInput>
+);
+
+Radio.propTypes = {
+  chooseStyle: PropTypes.string.isRequired,
+  style: PropTypes.string.isRequired
+};
+
+export default Radio;
