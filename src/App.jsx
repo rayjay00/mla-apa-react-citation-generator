c1181a06483770dc3a7a718252e1402ffc545c27
Ryan <hello@ryanjordal.me>
2019-06-16 12:09:00 -0400

    text update

--- src/App.jsx
+++ src/App.jsx
@@ -91,8 +91,8 @@ const App = () => {
       <Container style={style}>
         <Choose chooseStyle={chooseStyle} style={style} />
         <StyledAbout>
-          <a href='https://www.ryanjordal.me'>Built by Ryan</a>. See it
-          <a href='https://www.github.com/rayjay00'> here on GitHub.</a>
+          <a href='https://www.ryanjordal.me'>Built by Ryan</a>.
+          <a href='https://www.github.com/rayjay00'>See it here on GitHub.</a>
         </StyledAbout>
         <CitationForm
           handleSubmit={handleSubmit}
